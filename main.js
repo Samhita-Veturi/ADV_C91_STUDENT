d0f8ed309b8c480c79003eab93082fc5ef0857d4
Samhita-Veturi <67774716+Samhita-Veturi@users.noreply.github.com>
2021-01-15 22:27:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var Player_1 = document.getElementById("Ply_1").value;
-var Player_2 = document.getElementById("Ply_2").value;
+function AddUser(){
+    Play_1 = document.getElementById("Player_1").value;
+    Play_2 = document.getElementById("Player_2").value;
+    Feremy = localStorage.setItem("Player_1", Play_1);
+    Eremy = localStorage.setItem("Player_2", Play_2);
 
-function Login(){
-    localStorage.setItem("Player-1", Player_1);
-    localStorage.setItem("Player-2", Player_2);
     window.location = "Game.html";
 }
